48f3e9a94f7bc12d60898f0e82156a3c41be58df
loitp <www.muathu@gmail.com>
2023-04-02 12:54:10 +0700

    update

--- app/src/main/java/com/roy/speedtest/test/HttpUploadTest.kt
+++ app/src/main/java/com/roy/speedtest/test/HttpUploadTest.kt
@@ -1,145 +1,114 @@
-package com.roy.speedtest.test;
+package com.roy.speedtest.test
 
-import static com.roy.speedtest.test.HttpUploadTest.uploadedKByte;
+import java.io.DataOutputStream
+import java.math.BigDecimal
+import java.math.RoundingMode
+import java.net.URL
+import java.util.concurrent.Executors
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLSession
+import javax.net.ssl.SSLSocketFactory
 
-import java.io.DataOutputStream;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.net.URL;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLSocketFactory;
-
-public class HttpUploadTest extends Thread {
-
-    public String fileURL;
-    static int uploadedKByte = 0;
-    double uploadElapsedTime = 0;
-    boolean finished = false;
-    double elapsedTime = 0;
-    double finalUploadRate = 0.0;
-    long startTime;
-
-    public HttpUploadTest(String fileURL) {
-        this.fileURL = fileURL;
+class HttpUploadTest(private var fileURL: String) : Thread() {
+    companion object {
+        var uploadedKByte = 0
     }
 
-    private double round(double value, int places) {
-        if (places < 0) throw new IllegalArgumentException();
+    private var uploadElapsedTime = 0.0
+    var isFinished = false
+    private var elapsedTime = 0.0
+    private var finalUploadRate = 0.0
+    private var startTime: Long = 0
 
-        BigDecimal bd;
-        try {
-            bd = new BigDecimal(value);
-        } catch (Exception ex) {
-            return 0.0;
+    private fun round(value: Double, places: Int): Double {
+        require(places >= 0)
+        var bd: BigDecimal = try {
+            BigDecimal(value)
+        } catch (ex: Exception) {
+            return 0.0
         }
-        bd = bd.setScale(places, RoundingMode.HALF_UP);
-        return bd.doubleValue();
+        bd = bd.setScale(places, RoundingMode.HALF_UP)
+        return bd.toDouble()
     }
 
-    public boolean isFinished() {
-        return finished;
-    }
-
-    public double getInstantUploadRate() {
-        try {
-            BigDecimal bd = new BigDecimal(uploadedKByte);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            return 0.0;
-        }
-
-        if (uploadedKByte >= 0) {
-            long now = System.currentTimeMillis();
-            elapsedTime = (now - startTime) / 1000.0;
-            return round((Double) (((uploadedKByte / 1000.0) * 8) / elapsedTime), 2);
-        } else {
-            return 0.0;
-        }
-    }
-
-    public double getFinalUploadRate() {
-        return round(finalUploadRate, 2);
-    }
-
-    @Override
-    public void run() {
-        try {
-            URL url = new URL(fileURL);
-            uploadedKByte = 0;
-            startTime = System.currentTimeMillis();
-
-            ExecutorService executor = Executors.newFixedThreadPool(4);
-            for (int i = 0; i < 4; i++) {
-                executor.execute(new HandlerUpload(url));
+    val instantUploadRate: Double
+        get() {
+            try {
+                BigDecimal(uploadedKByte)
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                return 0.0
             }
-            executor.shutdown();
-            while (!executor.isTerminated()) {
+            return if (uploadedKByte >= 0) {
+                val now = System.currentTimeMillis()
+                elapsedTime = (now - startTime) / 1000.0
+                round((uploadedKByte / 1000.0 * 8 / elapsedTime), 2)
+            } else {
+                0.0
+            }
+        }
+
+    fun getFinalUploadRate(): Double {
+        return round(finalUploadRate, 2)
+    }
+
+    override fun run() {
+        try {
+            val url = URL(fileURL)
+            uploadedKByte = 0
+            startTime = System.currentTimeMillis()
+            val executor = Executors.newFixedThreadPool(4)
+            for (i in 0..3) {
+                executor.execute(HandlerUpload(url))
+            }
+            executor.shutdown()
+            while (!executor.isTerminated) {
                 try {
-                    Thread.sleep(100);
-                } catch (InterruptedException ex) {
-                    ex.printStackTrace();
+                    sleep(100)
+                } catch (ex: InterruptedException) {
+                    ex.printStackTrace()
                 }
             }
-
-            long now = System.currentTimeMillis();
-            uploadElapsedTime = (now - startTime) / 1000.0;
-            finalUploadRate = (Double) (((uploadedKByte / 1000.0) * 8) / uploadElapsedTime);
-
-        } catch (Exception ex) {
-            ex.printStackTrace();
+            val now = System.currentTimeMillis()
+            uploadElapsedTime = (now - startTime) / 1000.0
+            finalUploadRate = (uploadedKByte / 1000.0 * 8 / uploadElapsedTime)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
         }
-
-        finished = true;
+        isFinished = true
     }
 }
 
-class HandlerUpload extends Thread {
-
-    URL url;
-
-    public HandlerUpload(URL url) {
-        this.url = url;
-    }
-
-    public void run() {
-        byte[] buffer = new byte[150 * 1024];
-        long startTime = System.currentTimeMillis();
-        int timeout = 8;
-
+internal class HandlerUpload(private var url: URL) : Thread() {
+    override fun run() {
+        val buffer = ByteArray(150 * 1024)
+        val startTime = System.currentTimeMillis()
+        val timeout = 8
         while (true) {
-
             try {
-                HttpsURLConnection conn;
-                conn = (HttpsURLConnection) url.openConnection();
-                conn.setDoOutput(true);
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                conn.setSSLSocketFactory((SSLSocketFactory) SSLSocketFactory.getDefault());
-                conn.setHostnameVerifier((hostname, session) -> true);
-                conn.connect();
-                DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
-
-
-                dos.write(buffer, 0, buffer.length);
-                dos.flush();
-
-                conn.getResponseCode();
-
-                uploadedKByte += buffer.length / 1024.0;
-                long endTime = System.currentTimeMillis();
-                double uploadElapsedTime = (endTime - startTime) / 1000.0;
+                val conn: HttpsURLConnection = url.openConnection() as HttpsURLConnection
+                conn.doOutput = true
+                conn.requestMethod = "POST"
+                conn.setRequestProperty("Connection", "Keep-Alive")
+                conn.sslSocketFactory = SSLSocketFactory.getDefault() as SSLSocketFactory
+                conn.hostnameVerifier = HostnameVerifier { _: String?, _: SSLSession? -> true }
+                conn.connect()
+                val dos = DataOutputStream(conn.outputStream)
+                dos.write(buffer, 0, buffer.size)
+                dos.flush()
+                conn.responseCode
+                HttpUploadTest.uploadedKByte += (buffer.size / 1024.0).toInt()
+                val endTime = System.currentTimeMillis()
+                val uploadElapsedTime = (endTime - startTime) / 1000.0
                 if (uploadElapsedTime >= timeout) {
-                    break;
+                    break
                 }
-
-                dos.close();
-                conn.disconnect();
-            } catch (Exception ex) {
-                ex.printStackTrace();
-                break;
+                dos.close()
+                conn.disconnect()
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+                break
             }
         }
     }
