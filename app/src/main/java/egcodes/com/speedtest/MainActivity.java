e6cf9d70e1677c01ae94638dc30466f5252a4be9
loitp <www.muathu@gmail.com>
2023-04-02 11:42:12 +0700

    update

--- app/src/main/java/egcodes/com/speedtest/MainActivity.java
+++ app/src/main/java/egcodes/com/speedtest/MainActivity.java
@@ -2,7 +2,7 @@ package egcodes.com.speedtest;
 
 import android.graphics.Color;
 import android.location.Location;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
